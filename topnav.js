7f830d11f7fe03fc6cd417cfe8f7fe8b88b8f410
Xorol <88636352+Xorol@users.noreply.github.com>
2024-03-21 22:39:22 -0400

    Fix - Missing Rparen (line 4)

--- topnav.js
+++ topnav.js
@@ -1,7 +1,7 @@
 //Inserts a navigation bar into every document linked to it
 //also adds the favicon
 
-window.onload = function(){document.head.append('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"><link rel="manifest" href="/site.webmanifest">'};
+window.onload = function(){document.head.append('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"><link rel="manifest" href="/site.webmanifest">')};
 
 const topnav = document.getElementsByClassName("topnav")[0];
 
